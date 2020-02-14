7e79af16bea68a0523087fe55336d6ed91e8483c
unknown <you@example.com>
2020-02-13 21:12:36 -0300

    adicionando url puc

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
 //baseURL:'http://192.168.1.145:3333' // Casa
-  baseURL:'http://10.255.0.181:3333' // Qintess
+  // baseURL:'http://10.255.0.181:3333' // Qintess
+  baseURL:'http://172.16.229.154:3333' // Pucexp://172.16.229.154:19000
 })
 
 export default api
